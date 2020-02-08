8f3966d4fa52e79006357a46773822961c497054
ThkPsss <christanspence66@gmail.com>
2020-02-07 21:31:52 -0500

    Starting to fix this

--- app/server.js
+++ app/server.js
@@ -7,3 +7,9 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
